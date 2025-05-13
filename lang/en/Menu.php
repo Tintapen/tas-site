6d893875eb7f7b22405919d4d2d42d0e529c1ede
Tintapen <okiper12@gmail.com>
2025-05-13 10:43:32 +0700

    feature : Header menu set language

---
+++ lang/en/Menu.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'MN1' => 'Home',
+    'MN2' => 'Company',
+    'MN3' => 'About Us',
+    'MN4' => 'Career',
+    'MN5' => 'Product',
+    'MN6' => 'Media',
+    'MN7' => 'News',
+    'MN8' => 'Gallery',
+    'MN9' => 'Contact',
+];
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,31 +8,31 @@
 
         <nav id="navmenu" class="navmenu">
           <ul>
-            <li><a href="{{ url('/') }}" class="{{ Request::is('/') ? 'active' : '' }}">Home</a></li>
+            <li><a href="{{ url('/') }}" class="{{ Request::is('/') ? 'active' : '' }}">{{ __('Menu.MN1') }}</a></li>
         
             <li class="dropdown">
               <a href="#" class="{{ Request::is('about-us') || Request::is('career') ? 'active' : '' }}">
-                <span>Company</span> <i class="bi bi-chevron-down toggle-dropdown"></i>
+                <span>{{ __('Menu.MN2') }}</span> <i class="bi bi-chevron-down toggle-dropdown"></i>
               </a>
               <ul>
-                <li><a href="{{ url('/about-us') }}" class="{{ Request::is('about-us') ? 'active' : '' }}">About Us</a></li>
-                <li><a href="{{ url('/career') }}" class="{{ Request::is('career') ? 'active' : '' }}">Career</a></li>
+                <li><a href="{{ url('/about-us') }}" class="{{ Request::is('about-us') ? 'active' : '' }}">{{ __('Menu.MN3') }}</a></li>
+                <li><a href="{{ url('/career') }}" class="{{ Request::is('career') ? 'active' : '' }}">{{ __('Menu.MN4') }}</a></li>
               </ul>
             </li>
         
-            <li><a href="{{ url('/product') }}" class="{{ Request::is('product') ? 'active' : '' }}">Product</a></li>
+            <li><a href="{{ url('/product') }}" class="{{ Request::is('product') ? 'active' : '' }}">{{ __('Menu.MN5') }}</a></li>
         
             <li class="dropdown">
               <a href="#" class="{{ Request::is('news') || Request::is('gallery') ? 'active' : '' }}">
-                <span>Media</span> <i class="bi bi-chevron-down toggle-dropdown"></i>
+                <span>{{ __('Menu.MN6') }}</span> <i class="bi bi-chevron-down toggle-dropdown"></i>
               </a>
               <ul>
-                <li><a href="{{ url('/news') }}" class="{{ Request::is('news') ? 'active' : '' }}">News</a></li>
-                <li><a href="{{ url('/gallery') }}" class="{{ Request::is('gallery') ? 'active' : '' }}">Gallery</a></li>
+                <li><a href="{{ url('/news') }}" class="{{ Request::is('news') ? 'active' : '' }}">{{ __('Menu.MN7') }}</a></li>
+                <li><a href="{{ url('/gallery') }}" class="{{ Request::is('gallery') ? 'active' : '' }}">{{ __('Menu.MN8') }}</a></li>
               </ul>
             </li>
         
-            <li><a href="{{ url('/contact') }}" class="{{ Request::is('contact') ? 'active' : '' }}">Contact</a></li>
+            <li><a href="{{ url('/contact') }}" class="{{ Request::is('contact') ? 'active' : '' }}">{{ __('Menu.MN9') }}</a></li>
           </ul>
           <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
         </nav>        
